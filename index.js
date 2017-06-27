2152d7a64b710c5f11eb7652d402a7727e5710ff
yshur <yair.shur@gmail.com>
2017-06-27 06:27:15 +0300

    remove headers

--- index.js
+++ index.js
@@ -7,14 +7,7 @@ const express = require ('express'),
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/includes', express.static(`${__dirname}/public`));
-app.use(
-(req,res,next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers",
-  "Origin, X-Requested-With, Content-Type, Accept");
-  res.set("Content-Type", "application/json");
-  next();
-});
+
 
 app.all('*', (req, res, next) => {
   console.log("runs for all HTTP verbs first");
